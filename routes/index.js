7d67ce5cce0d565bd8d09f3c6e9c382310a08968
Pushkar Anand <anandpushkar088@gmail.com>
2018-12-08 18:22:40 +0530

    added index route and page.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
 const express = require('express');
 const router = express.Router();
 
+const linksArray = [
+	{
+		title: "AAA",
+		link: "aaa"
+	},
+	{
+		title: "BBB",
+		link: "bbb"
+	}
+];
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+	const data = {
+		title: "iiDeo",
+		name: "iiDeo",
+		tagline: "",
+		links: linksArray
+	};
+	res.render('index', data);
 });
 
 module.exports = router;
